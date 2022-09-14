4b4d5e9e86997d3dba6593fca5d10640a07183fa
CarlosKim <dlsxody1@gmail.com>
2022-09-14 14:52:48 +0900

    design : 헤더 디자인 및 데이터 바인딩 완료

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@
 import { Routes, Route } from "react-router-dom";
 import IssueListPage from "./pages/IssueListPage";
 import IssueDetailPage from "./pages/IssueDetailPage";
-
+import Header from "./components/Header";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<IssueListPage />} />
-      <Route path="issue-detail/:number" element={<IssueDetailPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<IssueListPage />} />
+        <Route path="issue-detail/:number" element={<IssueDetailPage />} />
+      </Routes>
+    </>
   );
 }
 
